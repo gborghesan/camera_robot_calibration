23acfe338273519c326220b0ec5394b2f6426c0c
Gianni Borghesan <gianni.borghesan@mech.kuleuven.be>
2014-04-24 11:37:15 +0200

    cleaning up

--- scripts/camera_robot_calibration_module.py
+++ scripts/camera_robot_calibration_module.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python  
+"""
+Copyright (c) 2014, Gianni Borghesan 
+All rights reserved. 
 
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met: 
+
+ * Redistributions of source code must retain the above copyright notice, 
+   this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in the 
+   documentation and/or other materials provided with the distribution. 
+ * Neither the name of KU Leuven nor the names of its contributors may be 
+   used to endorse or promote products derived from this software without 
+   specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE. 
+"""
 import numpy as num
 from geometry_msgs.msg import Pose, Point, Quaternion
 from tf_conversions import posemath
@@ -31,7 +58,7 @@ def load_pose_from_file(fname):
 
 def create_A_B(w_T_ee, ee_T_mn,w_T_cn,cr_T_mr):
     """
-    Generates the A and B sub-matrices  for the optimal problem, for each set of poses
+    Generates the A and B sub-matrices  for the optimization problem, for each set of poses
     
     Parameters
     ----------
